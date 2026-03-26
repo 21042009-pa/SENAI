8f4daa0b4c7a4fa2256b81a9802d22b9b1960e30
21042009-pa <paula.gianotto@aluno.senai.br>
2026-03-26 13:06:41 -0300

    f

--- Front end/VsCode/EXERCICIO AULA 6/src/script/script.js
+++ Front end/VsCode/EXERCICIO AULA 6/src/script/script.js
@@ -1,8 +1,43 @@
-const botao = document.querySelector ('#btn-curtir')
-const contar = document.querySelector ('#contador')
-let contador = 0
+//1
+const btn = document.querySelector('#btn-curtir')
+const contador = document.querySelector('#contador')
+let contar = 0
 
-if (botao === 'click'){
-    contador ++
+btn.addEventListener('click', function () {
+    contar++
     contador.innerText = contar
-}
+})
+
+//2
+
+const texto = document.querySelector('#campo-texto')
+const preview = document.querySelector('#preview-texto')
+
+texto.addEventListener('keyup', function (){
+    let preview = texto.value()
+    console.log(preview)
+})
+
+
+
+//3
+
+const cor_muda = document.querySelector ('#caixa-cor')
+
+cor_muda.addEventListener('mouseenter', function(){
+    cor_muda.style.backgroundColor = 'blue'
+})
+
+cor_muda.addEventListener('mouseleave', function(){
+    cor_muda.style.backgroundColor = 'gray'
+})
+
+
+//4
+const reset = document.querySelector('#btn-reset')
+
+reset.addEventListener('click', function(){
+    contar = 0,
+    contador.textContext = 0
+    texto.value = ''
+})
